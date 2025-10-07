9b4f49b05a05f82cb87dd186662881422d367e5c
Patrick Perez <patrick_hans_perez@dlsu.edu.ph>
2025-10-08 02:03:21 +0800

    feat: added a supabase connection to the program

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-import sys
-import time
-import gc
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-def main():
-    print("Hello World!")
+# Load environment variables from .env
+load_dotenv()
 
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
 
-if __name__ == "main":
-    main()
+# Construct the SQLAlchemy connection string
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+#creates the engine to connect to supabase
+engine = create_engine(DATABASE_URL)
