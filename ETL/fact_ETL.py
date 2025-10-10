10de31858e6fbcd350c30f90c7e07371e36becc8
Patrick Perez <patrick_hans_perez@dlsu.edu.ph>
2025-10-10 11:04:21 +0800

    feat: added simple extraction to fact table

---
+++ ETL/fact_ETL.py
@@ -0,0 +1,22 @@
+from config import local
+from sqlalchemy import  select, automap_base
+import pandas as pd
+
+def extractFact():
+   engine = local.localConnect()
+   
+   Base = automap_base()
+   
+   Base.prepare(autoload_with=engine)
+
+   Product = Base.classes.products
+   OrderItem = Base.classes.orderitems
+   Order = Base.classes.orders
+   
+   #sa dataframe (transform) ilalagay ung surrogate keys
+   stmt = select(OrderItem.quantity, (OrderItem.quantity * Product.price).label('revenue'), Order.orderNumber).join(
+    Order, OrderItem.OrderId == Order.id)
+   
+   df = pd.read_sql(stmt, engine)
+   
+   return df
